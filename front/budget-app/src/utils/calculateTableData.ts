c5d1faf348d475ca3ea48591cdc3340553a3c488
BearGom2 <sjalaa@naver.com>
2025-09-12 00:01:35 +0900

    단위 수정

--- front/budget-app/src/utils/calculateTableData.ts
+++ front/budget-app/src/utils/calculateTableData.ts
@@ -11,7 +11,7 @@ export const calculateTableData = (histories: History[]) => {
       (acc, history) => {
         const amount = Number(history.amount);
         const isExpense = history.type === "expense";
-        const newBalance = isExpense ? acc.balance - amount : acc.balance + amount;
+        const newBalance = acc.balance + amount;
         const newTotal = acc.total + (isExpense ? amount : 0);
 
         acc.rows.push([
